fc84dc27ed443b4ea994207b3af865c5df5cb9bc
gabriel <gabrielmouraguimaraes@gmail.com>
2022-12-17 22:12:16 -0300

    resolvendo q1

--- fundamentos-js/2-arrays-js/1.0-/README.md
+++ fundamentos-js/2-arrays-js/1.0-/README.md
@@ -5,4 +5,4 @@
 #### Javascript Arrays
 Esta série de desafios testa seu conhecimento sobre as matrizes Javascript
 ###### Questão 
-
+Escreva uma função que receba um array (a) e um valor como argumento, e retorne o n-ésimo elemento de 'a' 
---
+++ fundamentos-js/2-arrays-js/1.0-/script.js
@@ -0,0 +1,11 @@
+function nthElement(a, n) {
+   const res = [];
+   
+    return res
+}
+
+
+//CASOS DE TESTE
+console.log(nthElement([1, 2, 3, 4, 5], 3))
+console.log(nthElement([10, 9, 8, 7, 6], 5))
+console.log(nthElement([7, 2, 1, 6, 3], 1))
