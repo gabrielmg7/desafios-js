250d14ff96b3c96acf2d60f236e6987084a2297a
gabrielmg7 <gabrielmdevops@gmail.com>
2023-01-22 14:07:49 -0300

    resolvendo

--- fundamentos-js/2-arrays-js/1.7-count-negative-values/script.js
+++ fundamentos-js/2-arrays-js/1.7-count-negative-values/script.js
@@ -1,1 +1,8 @@
-export {}
+function valoresNegativos(array) {
+    let negativeArray = array.filter(number => number < 0)
+    return negativeArray.length
+}
+
+console.log(valoresNegativos([1, -2, 2, -4]))
+console.log(valoresNegativos([0, 9, 1]))
+console.log(valoresNegativos([4, -3, 2, 1, 0]))
